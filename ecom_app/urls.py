2e3a6366c8973d447e80bee4b8195f9c6358585f
Prayush Neupane <pyprayushneupane@gmail.com>
2025-04-16 14:03:30 +0545

    added cart add, delete,update views

--- ecom_app/urls.py
+++ ecom_app/urls.py
@@ -3,13 +3,15 @@ from django.urls import path
 from ecom_app import views
 
 urlpatterns = [
+
+    # home page and others views
    path("", views.HomePage.as_view(), name="home"),
    path("about/",views.AboutPage.as_view(),name="about"),
    path("register/",views.UserResister.as_view(), name="register"),
    path("product/<int:id>",views.ProductDetail.as_view(), name="product-detail"),
    path("category/<str:id>", views.CategoryView.as_view(), name="category"),
 
-    #cart-section
+    #cart views
    path('cart/',views.MainCart.as_view(),name="cart_main"),
     path('add/',views.AddCart.as_view(),name="cart_add"),
     path('delete/',views.DeleteCart.as_view(),name="cart_delete"),
--- ecom_app/views.py
+++ ecom_app/views.py
@@ -1,13 +1,11 @@
+from django.http import JsonResponse
 from django.shortcuts import render,get_object_or_404
-from django.views.generic import ListView,CreateView,DetailView,DeleteView,View
+from django.views.generic import ListView,CreateView,DetailView,DeleteView
 from ecom_app.models import Customer, Product,Category,Cart
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-
-from django.http import JsonResponse
-from django.utils.decorators import method_decorator
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
 
 
 
@@ -54,20 +52,85 @@ class CategoryView(DetailView):
     
 
 #cart views
-class MainCart(ListView):
-    model = Cart
-    template_name = "cart/cart_main.html"
-    
-
-class AddCart(CreateView):
+class MainCart(View):
+    def get(self,request,*args,**kwargs):
+        customer = request.user.customer
+        cart_items = Cart.objects.filter(customer=customer)
+        total_price = sum([item.get_total_price() for item in cart_items]) 
+        return render(
+            request, 
+            "cart/cart_main.html",
+              {
+                "cart_items":cart_items,
+               "total_price":total_price
+               })
    
-   def (self, request, )
-
-class DeleteCart(DeleteView):
-    pass
-
-class UpdateCart(DetailView):
-   pass
-
-
     
+
+class AddCart(View):
+
+    def post(self,request,*args,**kwargs):
+        data = json.loads(request.body)
+        product_id = data.get('product_id')
+        customer = request.user.customer #  user is login then only
+
+        # to check if product exists
+        product = Product.objects.get(id = product_id)
+
+        # checking if product is already on cart
+        cart_item, created = Cart.objects.get_or_create(customer=customer, product=product)
+
+        if not created:
+            cart_item.quantity += 1 # to increase the quantity if it is already in cart
+            cart_item.save()
+
+        cart_count = Cart.objects.filter(customer=customer).count()
+
+        return JsonResponse({
+            "status":"success",
+            "quantity":cart_item.quantity,
+            "cart_count":cart_count
+            })
+
+
+
+class DeleteCart(View):
+
+    def post(self, request, *args, **kwargs):
+        data = json.loads(request.body)
+        cart_id = data.get('cart_id')
+
+        #  to Remove the cart item
+        cart_item = Cart.objects.get(id=cart_id)
+        cart_item.delete()
+
+        cart_count = Cart.objects.filter(customer=request.user.customer).count()
+       
+        return JsonResponse({
+            "status": "success",
+              "cart_count": cart_count
+              })
+
+
+
+
+class UpdateCart(View):
+    
+    def post(self, request, *args, **kwargs):
+        data = json.loads(request.body)
+        cart_id = data.get('cart_id')
+        new_quantity = data.get('quantity')
+
+        #  to Update cart item quantity
+        cart_item = Cart.objects.get(id=cart_id)
+        cart_item.quantity = new_quantity
+        cart_item.save()
+
+        cart_count = Cart.objects.filter(customer=request.user.customer).count()
+       
+        return JsonResponse({
+            "status": "success",
+              "quantity": cart_item.quantity,
+                "cart_count": cart_count
+                })
+
